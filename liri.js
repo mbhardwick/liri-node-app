1798030fa55c81a88db0dedfb2ee57e0609b7ef6
MB Hardwick <48660923+mbhardwick@users.noreply.github.com>
2019-10-02 13:46:28 -0700

    print to log.js

--- liri.js
+++ liri.js
@@ -5,18 +5,27 @@ var Spotify = require('node-spotify-api');
 var axios = require('axios');
 var keys = require("./keys.js");
 var moment = require('moment');
+//Output to log.txt
+var logThis = function(logCommand){
+    console.log(logCommand);
+    fs.appendFile('log.txt', logCommand, function(err){
+        if (err){
+            return logThis('Error: '+err);
+        }
+    })
+};
 //Concert-This
 var concertThis = function(artist){
     axios.get('https://rest.bandsintown.com/artists/'+artist+'/events?app_id=codingbootcamp')
     .then(function(response){
         for (var i=0;i<response.data.length; i++){
-            console.log('Venue Name: '+response.data[i].venue.name);
-            console.log('Venue Location: '+response.data[i].venue.city+', '+response.data[i].venue.region+' '+response.data[i].venue.country);
-            console.log('Concert Date: '+moment(response.data[i].datetime).format('L'));
-            console.log('-------------------------------------------------')
+            logThis('\n----------------------------------------')
+            logThis('\nVenue Name: '+response.data[i].venue.name);
+            logThis('\nVenue Location: '+response.data[i].venue.city+', '+response.data[i].venue.region+' '+response.data[i].venue.country);
+            logThis('\nConcert Date: '+moment(response.data[i].datetime).format('L'));
         }
-    });
-}
+    })
+};
 //Spotify-This-Song
 var spotify = new Spotify(keys.spotify);
 var artistNames = function(artist) {
@@ -29,19 +38,19 @@ var spotifyThis = function(songName){
     }
     spotify.search({type: 'track', query: songName}, function(err, data) {
         if (err) {
-            console.log('Error occurred: '+err);
+            logThis('Error occurred: '+err);
             return;
         }
         var songs = data.tracks.items;
         for(var i=0; i<songs.length; i++) {
-            console.log(i);
-            console.log('Artist(s): '+songs[i].artists.map(artistNames));
-            console.log('Song Name: '+songs[i].name);
-            console.log('Preview Song: '+songs[i].preview_url)
-            console.log('Album: '+songs[i].album.name);
-            console.log('-----------------------------------------------')
+            logThis('\n----------------------------------------')
+            logThis('\n'+i);
+            logThis('\nArtist(s): '+songs[i].artists.map(artistNames));
+            logThis('\nSong Name: '+songs[i].name);
+            logThis('\nPreview Song: '+songs[i].preview_url);
+            logThis('\nAlbum: '+songs[i].album.name);
         }
-    });
+    })
 };
 //Movie-This
 var movieThis = function(movieName){
@@ -51,23 +60,25 @@ var movieThis = function(movieName){
     }
     axios.get('http://www.omdbapi.com/?apikey=trilogy&t='+movieName)
     .then(function(response){
-            console.log('Title: '+response.data.Title);
-            console.log('Year: '+response.data.Year);
-            console.log('IMDB Rating: '+response.data.imdbRating);
-            console.log('Rotten Tomatoes Rating: '+response.data.tomatoRating);
-            console.log('Country: '+response.data.Country);
-            console.log('Language: '+response.data.Language);
-            console.log('Plot: '+response.data.Plot);
-            console.log('Actors: '+response.data.Actors);
+        logThis('\n----------------------------------------')
+            logThis('\nTitle: '+response.data.Title);
+            logThis('\nYear: '+response.data.Year);
+            logThis('\nIMDB Rating: '+response.data.imdbRating);
+            logThis('\nRotten Tomatoes Rating: '+response.data.tomatoRating);
+            logThis('\nCountry: '+response.data.Country);
+            logThis('\nLanguage: '+response.data.Language);
+            logThis('\nPlot: '+response.data.Plot);
+            logThis('\nActors: '+response.data.Actors);
         })
     };
 //Do-What-It-Says
 var doThis = function(){
     fs.readFile('random.txt', 'utf8', function(err, data){
         if (err){
-            console.log(err);
+            logThis(err);
         }
         var readArr = data.split(',');
+        logThis('\n----------------------------------------')
         spotifyThis(readArr[1]);
     })
 };
@@ -75,19 +86,23 @@ var doThis = function(){
 var pick = function (caseData, functionData) {
     switch (caseData) {
         case 'concert-this':
+            logThis('concert-this: ');
             concertThis(functionData);
             break;
         case 'spotify-this-song':
+            logThis('spotify-this-song: ');
             spotifyThis(functionData);
             break;
         case 'movie-this':
+            logThis('movie-this: ');
             movieThis(functionData);
             break;
         case 'do-what-it-says':
+            logThis('do-what-it-says: ');
             doThis(functionData);
             break;
         default:
-            console.log('LIRI does not know that');
+            logThis('LIRI does not know that');
     }
 };
 //Function to take in the arguments
